e4f795ce89967ea1b8f563dc0533586341c4f71b
umarbashirr <umarbashir93@outlook.com>
2021-10-04 22:55:33 +0530

    functionality Added

--- src/Review.js
+++ src/Review.js
@@ -3,8 +3,46 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  const currentPerson = 0;
-  const [person, setPerson] = useState(people[currentPerson]);
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+
+    if (number < 0) {
+      return people.length - 1;
+    }
+
+    return number;
+  };
+
+  const prevReviewHandler = (e) => {
+    e.preventDefault();
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const nextReviewHandler = (e) => {
+    e.preventDefault();
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomReviewHandler = (e) => {
+    e.preventDefault();
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === index) {
+      randomIndex = index + 1;
+    }
+    console.log(randomIndex);
+    setIndex(checkNumber(randomIndex));
+  };
 
   return (
     <main>
@@ -16,27 +54,25 @@ const Review = () => {
         <div className='container'>
           <article className='review'>
             <div className='img-container'>
-              <img
-                src={person.image}
-                alt={person.image}
-                className='person-img'
-              />
+              <img src={image} alt={name} className='person-img' />
               <span className='quote-icon'>
                 <FaQuoteRight />
               </span>
             </div>
-            <h3 className='author'>{person.name}</h3>
-            <p className='job'>{person.job}</p>
-            <p className='info'>{person.text}</p>
+            <h3 className='author'>{name}</h3>
+            <p className='job'>{job}</p>
+            <p className='info'>{text}</p>
             <div className='btn-container'>
-              <button className='prev-btn'>
+              <button className='prev-btn' onClick={prevReviewHandler}>
                 <FaChevronLeft />
               </button>
-              <button className='next-btn'>
+              <button className='next-btn' onClick={nextReviewHandler}>
                 <FaChevronRight />
               </button>
             </div>
-            <button className='random-btn'>Surprise Me</button>
+            <button className='random-btn' onClick={randomReviewHandler}>
+              Surprise Me
+            </button>
           </article>
         </div>
       </section>
